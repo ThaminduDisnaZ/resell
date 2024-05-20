e509dbb1a8d8c8bfbe394154c48cbf3cce59615a
Thamindu Disna <156172744+ThaminduDisnaZ@users.noreply.github.com>
2024-05-20 17:12:45 +0530

    heder & footer😒

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    
+<?php
+require "heder.php";
+?>
+
+
+
+
+<?php
+require "footer.php";
+?>
+</body>
+</html>
